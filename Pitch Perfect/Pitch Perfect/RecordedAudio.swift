1a42ab7f0af834c9447e092f9771badfc950ac20
Matthew Flood <hermannwest@gmail.com>
2015-03-08 21:55:00 -0600

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Matt Flood on 3/8/15.
+//  Copyright (c) 2015 Matthew Flood. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
